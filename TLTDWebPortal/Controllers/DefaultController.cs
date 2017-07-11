86a7659f8c031b6fd108abb0dc4952f279df8f2b
andrewhamili <rockr1521@gmail.com>
2017-07-11 14:21:59 +0800

    Test for Tan

---
+++ TLTDWebPortal/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TLTDWebPortal.Models;
+
+namespace TLTDWebPortal.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        ceutltdschedulerEntities db;
+        // GET: Default
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+        public ActionResult Login()
+        {
+            Session.Abandon();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginParam param)
+        {
+            db = new ceutltdschedulerEntities();
+            user_login_Result result = new user_login_Result();
+            result= db.user_login(param.username, param.password).First();
+
+            if((!String.IsNullOrEmpty(result.staff_fname) & !String.IsNullOrEmpty(result.staff_surname))){
+                Session["user"] = result.staff_fname + " " + result.staff_surname;
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ TLTDWebPortal/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TLTDWebPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Console.WriteLine("3guard");
+            if (String.IsNullOrEmpty(Session["user"].ToString()))
+            {
+                return RedirectToAction("Login", "Default");
+            }
+            return View();
+        }
+    }
+}
